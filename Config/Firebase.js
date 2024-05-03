aa943da0b04ed12e4f3ab0d752e18f697fe0b076
Uriaaaa <126378661+Uriadutu@users.noreply.github.com>
2024-05-03 19:26:49 +0800

    Update Firebase.js

--- Config/Firebase.js
+++ Config/Firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database"; 
 
 // Konfigurasi Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBAxmOv2c72odcdGQ2PlGZCIO2TD65iNK8",
-  authDomain: "nodemcu-fb601.firebaseapp.com",
-  databaseURL: "https://nodemcu-fb601-default-rtdb.firebaseio.com",
-  projectId: "nodemcu-fb601",
-  storageBucket: "nodemcu-fb601.appspot.com",
-  messagingSenderId: "702849587999",
-  appId: "1:702849587999:web:679b05fb7ba88033abee2a",
-  measurementId: "G-Y10ZZ9TMSF",
+  apiKey: "apiKey",
+  authDomain: "auth",
+  databaseURL: "dbUrl",
+  projectId: "pId",
+  storageBucket: "sBucket",
+  messagingSenderId: "sId",
+  appId: "apiID",
+  measurementId: "measurementId",
 };
 
 const app = initializeApp(firebaseConfig);
